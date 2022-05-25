de6f12b0ff439289c12e175539e6451d16c17a5d
Marco Ricci <mr.ricci79@gmail.com>
2022-05-25 09:08:14 +0100

    completed review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
